b616bda44c208dd7eb4cd39fb2ff7eb6744de493
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-08-02 15:58:22 +0300

    [TTS-86] Implement Auth Misc (#1)
    * Implement the Auth Service
    * Implement the Interceptor
    * Implement the Guard
    
    ---------
    
    https://psavchenkoad.atlassian.net/browse/TTS-86?atlOrigin=eyJpIjoiOGIxZjE0YzJjNzYzNGMyMDkwZjQ0YjQyYmExY2M4ZTAiLCJwIjoiaiJ9

---
+++ src/app/core/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private accessToken: string | null = null;
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string) {
+    return this.http.post<{ accessToken: string; refreshToken: string }>(
+      '/api/login',
+      { email, password }
+    );
+  }
+
+  storeTokens(access: string, refresh: string) {
+    this.accessToken = access;
+    localStorage.setItem('refreshToken', refresh);
+  }
+
+  getAccessToken(): string | null {
+    return this.accessToken;
+  }
+
+  logout() {
+    this.accessToken = null;
+    localStorage.removeItem('refreshToken');
+  }
+}
