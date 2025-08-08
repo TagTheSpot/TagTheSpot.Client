923e30f7f951ea9a26b7e1716b869b9993e41853
savchenkoaddev <p.savchenkoad.dev@gmail.com>
2025-08-08 11:30:46 +0300

    Implement the component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { RegisterComponent } from './features/auth/register/register.component';
 import { MainPageComponent } from './features/main/main-page.component';
+import { CitySpotsComponent } from './features/city/city-spots.component/city-spots.component';
 
 export const routes: Routes = [
     {
@@ -22,6 +23,6 @@ export const routes: Routes = [
     { 
         path: 'cities/:id/spots' ,
         pathMatch: 'full',
-        component: LoginComponent
+        component: CitySpotsComponent
     }
 ];
