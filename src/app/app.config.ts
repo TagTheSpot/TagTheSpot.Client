6b31d44dc8ebf8115a11812e88c577aecc31c6fc
savchenkoaddev <p.savchenkoad.dev@gmail.com>
2025-08-18 16:31:40 +0300

    Implement the interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/auth/auth.interceptor';
+import { serverErrorInterceptor } from './core/interceptors/server-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor, serverErrorInterceptor]))
   ]
 };
---
+++ src/app/core/interceptors/server-error.interceptor.ts
@@ -0,0 +1,18 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+
+export const serverErrorInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError((error) => {
+      if (error.status >= 500) {
+        router.navigate(['/server-error']);
+      }
+
+      return throwError(() => error);
+    })
+  );
+};
