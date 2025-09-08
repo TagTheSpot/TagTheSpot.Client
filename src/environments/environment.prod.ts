7b722949c3455bc2d5b9f20c9a190ece06d27fcd
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-09-08 12:41:04 +0300

    [TTS-158] Implement Sign In With Google for Client (#39)
    https://psavchenkoad.atlassian.net/browse/TTS-158?atlOrigin=eyJpIjoiMzYxMjYyOWQyMTc0NDg2MGJmYjAwYzg3MDZhOWYwZGEiLCJwIjoiaiJ9

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   userServiceUrl: 'https://tagthespot-gateway.azurewebsites.net/user',
   spotServiceUrl: 'https://tagthespot-gateway.azurewebsites.net/spot',
-  moderationServiceUrl: 'https://tagthespot-gateway.azurewebsites.net/moderation'
+  moderationServiceUrl: 'https://tagthespot-gateway.azurewebsites.net/moderation',
+  googleClientId: '259519796253-a4995iq6evmdnu3k7b1o7scbe4skll34.apps.googleusercontent.com'
 };
